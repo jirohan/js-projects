108f1ec5e68909f3354c3971d776bd99f457f7e4
Rohan Maharjan <hondazmr6@gmail.com>
2023-06-27 12:15:03 +0545

    countdown completed

--- countdown/js/index.js
+++ countdown/js/index.js
@@ -1,4 +1,4 @@
-const endDate = "1 July 2023 10:00 PM"
+const endDate = "27 June 2023 10:00 PM"
 
 document.getElementById("end-date").innerHTML = endDate;
 const inputs = document.querySelectorAll("input")
@@ -7,10 +7,20 @@ const clock = () => {
     const end = new Date(endDate);
     const now = new Date();
     const diff = (end - now)/1000;
-    console.log(diff);
-    inputs[0].value = Math.floor(diff /3600 / 24);
 
+    if (diff < 0) return;
     //convert into days;
+    inputs[0].value = Math.floor(diff /3600 / 24);
+    //convert into hours;
+    inputs[1].value = Math.floor((diff /3600) % 24);
+    inputs[2].value = Math.floor((diff /60) % 60);
+    inputs[3].value = Math.floor((diff) % 60);
+
 }
 
 clock()
+
+setInterval(() => {
+        clock()
+    }, 1000
+)

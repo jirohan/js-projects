afa26c7b021b4c8578ae10f07e2934dbd0649d5e
Rohan Maharjan <hondazmr6@gmail.com>
2023-06-28 17:44:23 +0545

    completed

--- Champions league Quiz/js/app.js
+++ Champions league Quiz/js/app.js
@@ -41,14 +41,67 @@ const questions = [
     }
 ] 
 
-let index = 0;
-const quesBox = document.getElementById("quesBox");
-const optionInputs = document.querySelectorAll('.options');
-const loadQuestion = () => {
-    const data = questions[index]
-    console.log(data);
-    quesBox.innerText = `${index+1}. ${data.que}`;
+let index = 0; //2
+let total = questions.length;//17
+let right = 0; //18
+let wrong = 0; //19
+const quesBox = document.getElementById("quesBox"); //5
+const optionInputs = document.querySelectorAll('.options'); //7
+const loadQuestion = () => { //1
+    if (index === total){ //29
+        return endQuiz();
+    }
+    reset(); //26
+
+    const data = questions[index] //3
+
+    quesBox.innerHTML = `${index+1}. ${data.que}`; //6
+    optionInputs[0].nextElementSibling.innerText = data.a; //8
+    optionInputs[1].nextElementSibling.innerText = data.b; //9
+    optionInputs[2].nextElementSibling.innerText = data.c; //10
+    optionInputs[3].nextElementSibling.innerText = data.d; //11
 }
 
+const submitQuiz = () => { //12
+    const data = questions[index];
+    const ans = getAnswer() //15
+    if(ans === data.correct){ //16
+        right++; //20
+    } else{ //21
+        wrong++; //22
+    }
+    index++; //24
+    loadQuestion(); //25
+    return; //23
+} 
+
+const getAnswer = () => { //13
+    let answer;
+    optionInputs.forEach( //14 takes call back
+        (input) => {
+            if(input.checked){
+                answer = input.value;
+
+            }
+        }
+    )
+    return answer;
+}
+
+const reset = () => { //27
+    optionInputs.forEach( //28 
+        (input) => {
+            input.checked = false;
+        }
+    )
+}
+
+const endQuiz = () => { //30
+    document.getElementById("box").innerHTML = `
+    <h3>Thank you for playing quiz</h3>
+    <h2>${right} / ${total} are correct.</h2>
+    `
+}; 
+
 //init call
-loadQuestion();
+loadQuestion(); //4

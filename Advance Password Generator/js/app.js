9945d8513e13144e18dfe423f7c3e889dfad1e3a
Rohan Maharjan <hondazmr6@gmail.com>
2023-06-29 15:04:18 +0545

    js working

--- Advance Password Generator/js/app.js
+++ Advance Password Generator/js/app.js
@@ -8,8 +8,8 @@ const passBox = document.getElementById("pass-box")
 const totalChar = document.getElementById("total-char")
 const upperInput = document.getElementById("upper-case")
 const lowerInput = document.getElementById("lower-case")
-const numberInput = document.getElementById("number-case")
-const symbolInput = document.getElementById("symbol-case")
+const numberInput = document.getElementById("numbers")
+const symbolInput = document.getElementById("symbols")
 
 const getRandomData = (dataSet) => {
     return dataSet[Math.floor(Math.random() * dataSet.length)]
@@ -18,6 +18,18 @@ const getRandomData = (dataSet) => {
 const generatePassword = (password = "") => {
     if(upperInput.checked){
         password += getRandomData(upperSet)
+    }
+    if(lowerInput.checked){
+        password += getRandomData(lowerSet)
+    }
+    if(numberInput.checked){
+        password += getRandomData(numberSet)
+    }
+    if(symbolInput.checked){
+        password += getRandomData(symbolSet)
+    }
+    if (password.length < totalChar.value) {
+        return generatePassword(password)
     }
     console.log(password)
 }
